151ffb9d0a538804614f9df720711b1d34e35cc2
Sunny <sunnysun2108@gmail.com>
2017-12-22 17:00:23 -0500

    handle webhook

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,3 +1,7 @@
+const _ = require('lodash');
+const Path = require('path-parser');
+const { URL } = rquire('url');
+
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -6,8 +10,48 @@ const Survey = mongoose.model('surveys');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+
+  app.get('/api/surveys', requireLogin, async (req, res) {
+    const surveys = await Survey.find({ _user: req.user.id }).select({ 
+      recipients: false 
+    });
+    
+    res.send(surveys);
+  });
+  
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for voting!!');
+  });
+  
+  app.post('/api/surveys/webhooks', (res, res) => {
+    const p = new Path('/api/surveys/:surveyId/:choice');
+    
+    _.chain(req.body)  //lodash chain!
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+        return { email, surveyId: match.surveyId, choice: match.choice};
+        }
+      })
+      .compact() //kick out undefined.
+      .uniqBy('email', 'surveyId')  //find unique
+      .each(({ serveyId, email, choice}) => {  //de structure event
+        Survey.updateOne(  //update statement in mongodb
+          {
+            _id: surveyId,  //id should be _id, mongo rule
+            recipients: {
+              $elemMatch: { email: email, responded: false }
+            }
+          }, {
+            $inc: { [choice]: 1 },
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date()
+          }
+        ).exec();  //call exec() to execute the query.
+      })
+      .value();
+    
+    res.send({});
   });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
